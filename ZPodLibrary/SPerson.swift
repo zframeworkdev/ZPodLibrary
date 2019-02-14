5c72b7b0dd19a409cb4ced2553d791626a3a3d22
Zaldy <zframeworkdev@gmail.com>
2019-02-14 15:56:30 +0800

    Initial commit

---
+++ ZPodLibrary/SPerson.swift
@@ -0,0 +1,13 @@
+//
+//  SPerson.swift
+//  ZPodLibrary
+//
+//  Created by Zaldy on 14/02/2019.
+//  Copyright © 2019 InternetBrands. All rights reserved.
+//
+
+import Foundation
+
+func hero() {
+    
+}
---
+++ ZPodLibrary/ZPodLibrary.h
@@ -0,0 +1,19 @@
+//
+//  ZPodLibrary.h
+//  ZPodLibrary
+//
+//  Created by Zaldy on 14/02/2019.
+//  Copyright © 2019 InternetBrands. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ZPodLibrary.
+FOUNDATION_EXPORT double ZPodLibraryVersionNumber;
+
+//! Project version string for ZPodLibrary.
+FOUNDATION_EXPORT const unsigned char ZPodLibraryVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ZPodLibrary/PublicHeader.h>
+
+
