33b1e0ef21f6b70189b7db99a699a2b7b92e83aa
Zaldy <zframeworkdev@gmail.com>
2019-02-14 16:26:11 +0800

    added new library

---
+++ ZPodLilbraryB/SStudent.swift
@@ -0,0 +1,13 @@
+//
+//  SStudent.swift
+//  ZPodLilbraryB
+//
+//  Created by Zaldy on 14/02/2019.
+//  Copyright © 2019 InternetBrands. All rights reserved.
+//
+
+import Foundation
+
+func student() {
+    
+}
---
+++ ZPodLilbraryB/ZPodLilbraryB.h
@@ -0,0 +1,19 @@
+//
+//  ZPodLilbraryB.h
+//  ZPodLilbraryB
+//
+//  Created by Zaldy on 14/02/2019.
+//  Copyright © 2019 InternetBrands. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ZPodLilbraryB.
+FOUNDATION_EXPORT double ZPodLilbraryBVersionNumber;
+
+//! Project version string for ZPodLilbraryB.
+FOUNDATION_EXPORT const unsigned char ZPodLilbraryBVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ZPodLilbraryB/PublicHeader.h>
+
+
